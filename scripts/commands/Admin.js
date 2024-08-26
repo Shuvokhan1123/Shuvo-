91a9e89d85d0bd0add049b0a4b0d48b99524ad48
Minicat22 <skshakil22x@gmail.com>
2024-08-26 19:41:55 +0600

    Update Admin.js

--- scripts/commands/Admin.js
+++ scripts/commands/Admin.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-    name: "info2",
+    name: "admin",
     version: "1.0.0",
     permission: 0,
     credits: "nayan",
@@ -39,8 +39,8 @@ var callback = () => api.sendMessage({body:`
 𝐆𝐦𝐚𝐢𝐥       : xaikotom0@gmail.com
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/01918546009
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/.......
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/ceokuangtomyi`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+Photo 𝐋𝐢𝐧𝐤 : `https://scontent.xx.fbcdn.net/v/t1.15752-9/456085387_1073674757442108_7094234074580174013_n.jpg?stp=dst-jpg_p480x480&_nc_cat=110&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeHVp31AcHrV4llZLJ1WQGw-Vs2txin1cpBWza3GKfVykHoZEnUJlJuTYbNpL_d00HDi1md2d_ZnK0s-tTgi4sUw&_nc_ohc=qTMesNtFy7IQ7kNvgGBigeV&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QEfSxMFcXOxCQUnXQPJLRptIL05JojfdJQi5gfE-IgQTg&oe=66F3FD02,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI('https://cdn.discordapp.com/attachments/1277618478848675942/1277619409761992815/1712831704855.jpg?ex=66cdd34c&is=66cc81cc&hm=5687c193ccd92e1e56e5252de1a8c46bc1899d5781f6ea6b7bdac49ea94f9a2c&/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI('jpg?ex=66cdd34c&is=66cc81cc&hm=5687c193ccd92e1e56e5252de1a8c46bc1899d5781f6ea6b7bdac49ea94f9a2c&/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
