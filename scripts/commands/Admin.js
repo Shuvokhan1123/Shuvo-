9d9cd99b0eaf23e3c62f1b3cd88698ba2a50c806
Minicat22 <skshakil22x@gmail.com>
2024-08-26 19:34:14 +0600

    Create Admin.js

---
+++ scripts/commands/Admin.js
@@ -0,0 +1,46 @@
+module.exports.config = {
+    name: "Admin",
+    version: "1.0.0",
+    permission: 0,
+    credits: "nayan",
+    prefix: true,
+    description: "",
+    category: "prefix",
+    usages: "",
+    cooldowns: 5,
+    dependencies: 
+	{
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+const time = process.uptime(),
+		hours = Math.floor(time / (60 * 60)),
+		minutes = Math.floor((time % (60 * 60)) / 60),
+		seconds = Math.floor(time % 60);
+const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
+  
+var callback = () => api.sendMessage({body:`
+--------------------------------------------
+𝐍𝐚𝐦𝐞       : 🥵SK SHAKIL(TOM) 
+𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : Islam
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Rangpur, Bangladesh
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Rangpur.Lalmonirhat. patgram
+𝐆𝐞𝐧𝐝𝐞𝐫.   : Male
+𝐀𝐠𝐞           : 17+
+𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : Single Life Is Vest🥵💌
+𝐖𝐨𝐫𝐤        : College 
+𝐆𝐦𝐚𝐢𝐥       : xaikotom0@gmail.com
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/01918546009
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/.......
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/ceokuangtomyi`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+    fs.unlinkSync(__dirname + "/cache/1.png"));  
+      return request(encodeURI('https://cdn.discordapp.com/attachments/1277618478848675942/1277619409761992815/1712831704855.jpg?ex=66cdd34c&is=66cc81cc&hm=5687c193ccd92e1e56e5252de1a8c46bc1899d5781f6ea6b7bdac49ea94f9a2c&/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
+   };
